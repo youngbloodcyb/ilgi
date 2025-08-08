0114b20f60fad06d60f06c13692649beeeb7fe79
youngbloodcyb <cameron.youngblood@gmail.com>
2025-08-08 10:26:21 -0700

    add resizable

---
+++ components/dash/index.tsx
@@ -0,0 +1,38 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
+
+export function Dashboard() {
+  return (
+    <div className="p-4 h-full w-full">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="rounded-lg border h-full w-full"
+      >
+        <ResizablePanel defaultSize={75}>
+          <div className="flex items-center justify-center p-6">
+            <span className="font-semibold">One</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={25}>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel defaultSize={25}>
+              <div className="flex h-full items-center justify-center p-6">
+                <span className="font-semibold">Two</span>
+              </div>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel defaultSize={75}>
+              <div className="flex h-full items-center justify-center p-6">
+                <span className="font-semibold">Three</span>
+              </div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+}
